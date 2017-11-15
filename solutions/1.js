b2509592e8a536b072b536dc8211d1c5e1ef0e71
Benjamin Cooper <bencooper222@gmail.com>
2017-11-14 20:45:21 -0600

    changed var names

--- solutions/1.js
+++ solutions/1.js
@@ -1,22 +1,12 @@
-const fs = require('fs');
-
-
+let fs = require('fs');
 fs.readFile('../inputs/example1.json', 'utf8', function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    let r = JSON.parse(data);
-    console.log(invalids(r));
-});
-
-function invalids(r) {
-    let b = r['bans'];
-    let d = r['devices'];
-    let deleted = [];
-    for (let i = 0; i < b.length; i++) {
-        for (let j = 0; j < d.length; j++) {
-            if (d[j][b[i]["key"]] === b[i]["value"]) deleted.push(d[j]);
-        }
-    }
-    return new Set(deleted).size;
-}
+let r = JSON.parse(data);
+fs.writeFile('../answers/1.txt',test(r),function(err){});});
+function test(r) {
+let b = r['bans'];
+let d = r['devices'];
+let deleted = [];
+for (let i = 0; i < b.length; i++) {
+for (let j = 0; j < d.length; j++) {
+if (d[j][b[i]["key"]] === b[i]["value"]) deleted.push(d[j]);
+}}return new Set(deleted).size;}
