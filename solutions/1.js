4960c6c883651b6132581f0adfe3e95b18aae536
Benjamin Cooper <bencooper222@gmail.com>
2017-11-14 20:47:34 -0600

    awful code ftw

--- solutions/1.js
+++ solutions/1.js
@@ -1,12 +1,12 @@
 let fs = require('fs');
-fs.readFile('../inputs/example1.json', 'utf8', function(err, data) {
+fs.readFile('../inputs/example1.json','utf8',function(err,data) {
 let r = JSON.parse(data);
 fs.writeFile('../answers/1.txt',test(r),function(err){});});
 function test(r) {
 let b = r['bans'];
 let d = r['devices'];
 let q = [];
-for (let i = 0; i < b.length; i++) {
-for (let j = 0; j < d.length; j++) {
-if (d[j][b[i]["key"]] === b[i]["value"]) q.push(d[j]);
+for(let i=0;i<b.length;i++){
+for(let j=0;j<d.length;j++){
+if(d[j][b[i]["key"]]==b[i]["value"]) q.push(d[j]);
 }}return new Set(q).size;}
